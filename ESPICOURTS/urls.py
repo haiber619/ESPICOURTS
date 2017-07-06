26d0c2f6c184ebbbc22ad09b74dc3ada1926aa20
haiber619 <hgalindo15@itfip.edu.co>
2017-07-05 20:45:15 -0500

    template login

--- ESPICOURTS/urls.py
+++ ESPICOURTS/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^espicourts/', include('apps.inicio.urls', namespace='espicourts')),
+    url(r'^',login, {'template_name':'index.html'},name='login')
 ]
